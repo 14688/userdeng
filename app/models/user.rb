a0417318210cc6ab2407545e3b9c8c5a13b2aafd
14688 <18810636007@163.com>
2019-04-23 09:47:30 +0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+	protected
+	def email_required?
+		false
+	end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
